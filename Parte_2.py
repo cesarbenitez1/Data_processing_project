ecdbf6197170a33a8299a6c42b1d5ab4e34e4440
cesarbenitez1 <cesar.benitezv92@gmail.com>
2023-11-12 00:51:19 -0500

    .

--- Parte_2.py
+++ Parte_2.py
@@ -6,6 +6,19 @@ from datasets import load_dataset
 dataset = load_dataset("mstz/heart_failure")
 data = dataset["train"]
 
+#edades de los pacientes
+edades = data["age"]
+
+#convertir la lista de edades en un arreglo de numpy
+edades_np= np.array(edades)
+
+#calculo para el promedio de las edades
+promedio_edad=np.mean(edades_np)
+
+#imprimir el promedio de las edades
+print("El promedio de las edades es:", promedio_edad)
+
+#------------------PARTE 2---------------------------------
 # Convertir la estructura Dataset en un DataFrame de Pandas
 df = pd.DataFrame(data)
 
