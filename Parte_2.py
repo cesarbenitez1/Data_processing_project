8eb543efc1b59dfa85b15f40715e5d47f3cec21a
cesarbenitez1 <cesar.benitezv92@gmail.com>
2023-11-12 00:07:33 -0500

    Se agrega parte 2

---
+++ Parte_2.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+from datasets import load_dataset
+
+# Cargar el dataset
+dataset = load_dataset("mstz/heart_failure")
+data = dataset["train"]
+
+# Convertir la estructura Dataset en un DataFrame de Pandas
+df = pd.DataFrame(data)
+
+# Separar el dataframe en dos, uno con personas que perecieron y otro con el complemento
+df_perecieron = df[df["is_dead"] == 1]
+df_no_perecieron = df[df["is_dead"] == 0]
+
+# Calcular el promedio de edades para cada dataset
+promedio_edad_perecieron = df_perecieron["age"].mean()
+promedio_edad_no_perecieron = df_no_perecieron["age"].mean()
+
+# Imprimir los resultados
+print(f"Promedio de edad de personas que perecieron: {promedio_edad_perecieron} años")
+print(f"Promedio de edad de personas que no perecieron: {promedio_edad_no_perecieron} años")
